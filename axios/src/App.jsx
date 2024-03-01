011fe3bba97f30bbb6d851d9149a33dccd2d78e9
Akshata <akshata.metsa.4work@gmail.com>
2024-03-01 10:46:15 -0800

    implement react-bootstrap table

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import FetchData  from './FetchData'
 function App() {
+ 
   
   return (
+    <div className='App'>
+        < FetchData/>
+        <Table/> 
+       
+
+    </div>
     
-       < FetchData/> 
+      
   )
 }
 
--- axios/src/FetchData.jsx
+++ axios/src/FetchData.jsx
@@ -1,45 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
- function FetchData() {
-  const [data, setData] = useState([])
-    useEffect(()=> {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-        .then(res => setData(res.data))
-        .catch(err => console.log(err));
+function FetchData() {
+  const [data, setData] = useState([]);
 
-    },[])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className='container'>
-        <div className='mt-3'>
-          <h3>Fetch Data from API in React with</h3>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Body</th>
-               
-              </tr>
-            </thead>
-            <tbody>
-              {
-                data.map((user,index) => {
-                  return <tr key={index}>
-                    <td>{user.id}</td>
-                    <td>{user.title}</td>
-                    <td>{user.body}</td>
-                    
-                  </tr>
-                })
-              }
-            </tbody>
-          </table>
+      <div className='mt-3'>
+        <h3>Fetch Data from API in React with Bootstrap</h3>
 
-        </div>
+        <Table striped bordered hover >
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+              <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.title}</td>
+                <td>{user.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
-  )
+  );
 }
+
 export default FetchData;
